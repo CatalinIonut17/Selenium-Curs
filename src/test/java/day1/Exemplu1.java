3a447e170c91781b1d8090e33a69a947c81034dd
CatalinIonut17 <catalinionut096@gmail.com>
2022-08-05 10:52:51 +0300

    Initial commit

---
+++ src/test/java/day1/Exemplu1.java
@@ -0,0 +1,26 @@
+package day1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Exemplu1 {
+
+    public static void main(String[] args) {
+        /*System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe" );
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.endava.com/"); */
+
+        /*System.setProperty("webdriver.edge.driver", "src/test/resources/msedgedriver.exe" );
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://www.endava.com/");*/
+
+        System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe" );
+        WebDriver driver1 = new FirefoxDriver();
+        driver1.get("https://www.endava.com/");
+
+
+
+    }
+}
